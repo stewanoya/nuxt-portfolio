85f3afde44612d21e772e4f6562c877e16e7d88b
Stew Anoya <stewartanoya@gmail.com>
2022-02-04 12:09:14 -0500

    add real data to store and style cards

--- components/Home/Projects.vue
+++ components/Home/Projects.vue
@@ -6,13 +6,21 @@
         v-for="project in projectList"
         :key="project.id"
       >
-        <div class="project-header" :style="{ backgroundImage: project.image }">
+        <div class="project-header">
           <h1 class="project-title">{{ project.name }}</h1>
-          <!-- <img class="project-image" src="" /> -->
+          <img class="project-image" :src="project.image" />
         </div>
         <div class="project-body">
+          <a
+            :href="project.github"
+            class="project-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            ><img
+              class="github-icon"
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          /></a>
           <p class="project-description">{{ project.desc }}</p>
-          <p class="project-link">{{ project.github }}</p>
         </div>
       </article>
     </div>
@@ -43,20 +51,107 @@ export default {
   background-color: white;
   margin: 1rem;
   border-radius: 0.25rem;
+  height: 25rem;
+  position: relative;
 }
 
 .project-header {
-  padding: 1rem 2rem;
   width: 100%;
+  height: 9.5rem;
   background-color: #ffbe5e;
   text-align: center;
   font-family: 'Red Hat Text';
   font-weight: 600;
-  font-size: 1.5rem;
+  font-size: 1.75rem;
   color: white;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  border-radius: 0.25rem 0.25rem 0 0;
+}
+
+.project-title {
+  width: 100%;
+  color: white;
+  position: relative;
+  z-index: 10;
+}
+
+.project-image {
+  z-index: 0;
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: 0;
+}
+
+.project-header::before {
+  content: '';
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(3, 4, 15, 0.404);
+  z-index: 1;
+}
+
+.project-header::after {
+  content: '';
+  position: absolute;
+  height: 7rem;
+  width: 25rem;
+  bottom: -5rem;
+  left: -3rem;
+  border-radius: 50%;
+  background-color: rgb(255, 255, 255);
+  z-index: 1;
+  box-shadow: 0 -1rem 3rem 0.05rem rgba(15, 15, 15, 0.562);
 }
 
 .project-body {
   padding: 1rem 2rem;
+  height: 14.5rem;
+  overflow: hidden;
+  overflow-y: scroll;
+  position: relative;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.project-body::-webkit-scrollbar {
+  display: none;
+}
+
+.project-card::before {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 15%;
+  width: 100%;
+  background: linear-gradient(
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.575),
+    white
+  );
+  z-index: 10;
+}
+
+.project-description {
+  font-family: 'Quicksand';
+  font-weight: 500;
+  color: #413f4c;
+}
+
+.project-link {
+  color: #ffbe5e;
+  width: max-content;
+  display: block;
+  margin: 0.5rem auto;
+  margin-top: -1rem;
+}
+.github-icon {
+  width: 3rem;
+  aspect-ratio: 1;
 }
 </style>
--- store/projects.js
+++ store/projects.js
@@ -5,16 +5,18 @@ export const state = () => ({
     {
       id: 1,
       name: 'sQuiz Game',
-      image: 'this will be an image',
-      desc: 'this will be the description',
-      github: 'This will be a url',
+      image:
+        'https://raw.githubusercontent.com/stewanoya/sQuiz-Game/master/docs/homepage.png',
+      desc: 'sQuiz Game is an app that allows users to make quizzes, take quizzes and share results with friends. A user can search for quizzes, as well as make their quizzes public or private. Users can see the results of their recent attempt and share that result with friends.',
+      github: 'https://github.com/stewanoya/sQuiz-Game',
     },
     {
       id: 2,
       name: 'willow',
-      image: 'this will be an image',
-      desc: 'this will be the description',
-      github: 'This will be a url',
+      image:
+        'https://raw.githubusercontent.com/HabonH/Final-Demo/main/docs/dashboard-desktop.png',
+      desc: "Willow is a solution to help combat the lack of supports given to students suffering from poor mental health. The pandemic disruptions have proven how detrimental they can be to student wellness. The routine disruptions, adverse learning conditions and anti-social patterns can be difficult to handle when you aren't given tools to remedy them. Willow aims to be that set of tools.",
+      github: 'https://github.com/stewanoya/willow',
     },
   ],
 })
